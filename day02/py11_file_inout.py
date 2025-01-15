3080360b091fb50607bec653861404add1edadfd
BojungChoi <fake5378@naver.com>
2025-01-15 16:22:26 +0900

    다시

--- day02/py11_file_inout.py
+++ day02/py11_file_inout.py
@@ -2,10 +2,10 @@
 
 # 쓰기
 # a(추가) r(읽기) w(쓰기)
-f = open('./day02/textfile.txt', mode='w', encoding='utf-8') # ./day02/ 를 붙히면 day02 파일 내에 txt 파일이 들어감 , incoding을 해줘야 글이 안깨짐 (���� �ѱ�����Դϴ�.)
+f = open('./day02/textfile.txt', mode='w', encoding='utf-8') # ./day02/ 를 붙히면 day02 파일 내에 txt 파일이 들어감 , incoding을 해줘야 글이 안깨짐
 # 아무것도 안함
 f.write('나는 한국사람입니다.\n') 
-f.write('남자입니다.')
+f.write('남자입니다.\n')
 f.close()
 
 print('텍스트파일이 생성되었습니다.')
